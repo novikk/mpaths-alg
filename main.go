4a443900225e95fbf2c48302e1ebc92697d3653e
Ivan de la Rubia <ivandelarubia@gmail.com>
2017-02-11 15:32:42 +0100

    calling jar v1

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"math/rand"
+	"os/exec"
+	"strings"
 	"time"
+
+	"strconv"
 
 	"github.com/novikk/mpaths-alg/algorithm"
 	"github.com/novikk/mpaths-alg/models"
@@ -19,5 +24,33 @@ func main() {
 	clusters := algorithm.KmeansMaxDist(pts, 200)
 	for _, c := range clusters {
 		fmt.Println(c.Centroid, "-->", c.Pts, "(", c.Radius, ")")
+	}
+
+	// call algorithm in java
+	// build stdin
+	var stdin bytes.Buffer
+	stdin.WriteString(strconv.Itoa(len(clusters)) + "\n")
+
+	// sum := 0
+	for i := range clusters {
+		latStr := strconv.FormatFloat(clusters[i].Centroid.Lat, 'f', -1, 64)
+		lngStr := strconv.FormatFloat(clusters[i].Centroid.Lng, 'f', -1, 64)
+
+		stdin.WriteString(latStr + " " + lngStr + " " + strconv.Itoa(len(clusters[i].Pts)) + "\n")
+		// sum += len(clusters[i].Pts)
+	}
+
+	// fmt.Println(stdin.String())
+	// println(sum)
+
+	path, _ := exec.LookPath("java")
+	cmd := exec.Command(path, "-cp", "vrp-hackathon.jar:.", "hackathon.hackathon.RunVRP")
+	cmd.Dir = "."
+	cmd.Stdin = strings.NewReader(stdin.String())
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(string(output[:]), err)
+		return
 	}
 }
